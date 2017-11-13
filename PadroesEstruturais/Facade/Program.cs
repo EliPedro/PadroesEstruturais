1d1fc98f14d57cbbb10ee59131921177e6234323
EliPedro <elipedrodos.santos@gmail.com>
2017-11-13 11:19:11 -0200

    Refatoring

--- PadroesEstruturais/Facade/Program.cs
+++ PadroesEstruturais/Facade/Program.cs
@@ -18,15 +18,13 @@ namespace Facade
         private static void Execute()
         {
 
-            var cliente = new Cliente("Teste");
-            var produto    = new Produto(Guid.NewGuid(), "Óculos", "Óculos de Sol", 120M, DateTime.Now.AddDays(30), 2);          
-            var transporte = new Transporte();
-            var financeiro = new Financeiro();
-            var estoque    = new Estoque();
-            var endereco = new Endereco("06142230","xxx","xx","xx");
-
-            var pedido = new Pedido(transporte, estoque, financeiro, endereco, produto, cliente);
-
+            var cliente      = new Cliente("Teste");
+            var produto      = new Produto(Guid.NewGuid(), "Óculos", "Óculos de Sol", 120M, DateTime.Now.AddDays(30), 2);          
+            var transporte   = new Transporte();
+            var financeiro   = new Financeiro();
+            var estoque      = new Estoque();
+            var endereco     = new Endereco("06142230","xxx","xx","xx");
+            var pedido       = new Pedido(transporte, estoque, financeiro, endereco, produto, cliente);
             var pedidoFacade = new PedidoFacade(pedido);
             pedidoFacade.RegistraPedido();
 
